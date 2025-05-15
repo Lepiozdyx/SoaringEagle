f2e93e8dcc9df94ac18f82158247823020916633
Developer <Lepiozdyx@gmail.com>
2025-05-15 10:21:27 -0300

    Initial Commit

---
+++ SoaringEagle/SoaringEagleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoaringEagleApp.swift
+//  SoaringEagle
+//
+//  Created by Alex on 15.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct SoaringEagleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
