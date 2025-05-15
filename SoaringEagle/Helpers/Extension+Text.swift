064fc82e686d4089372c2626424e3acbf5f4da28
Developer <Lepiozdyx@gmail.com>
2025-05-15 10:50:19 -0300

    setup project, add assets

---
+++ SoaringEagle/Helpers/Extension+Text.swift
@@ -0,0 +1,33 @@
+//
+//  Extension+Text.swift
+//  SoaringEagle
+//
+//  Created by Alex on 15.05.2025.
+//
+
+import SwiftUI
+
+struct Extension_Text: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .tFont(32)
+    }
+}
+
+extension Text {
+    func tFont(_ size: CGFloat) -> some View {
+        let baseFont = UIFont(name: "Titan One", size: size) ?? UIFont.systemFont(ofSize: size, weight: .heavy)
+        let scaledFont = UIFontMetrics(forTextStyle: .body).scaledFont(for: baseFont)
+
+        return self
+            .font(Font(scaledFont))
+            .foregroundStyle(.white)
+            .shadow(color: .black, radius: 1)
+            .multilineTextAlignment(.center)
+            .textCase(.uppercase)
+    }
+}
+
+#Preview {
+    Extension_Text()
+}
---
+++ SoaringEagle/Screens/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  SoaringEagle
+//
+//  Created by Alex on 15.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .tFont(32)
+    }
+}
+
+#Preview {
+    ContentView()
+}
